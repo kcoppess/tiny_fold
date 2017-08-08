6c9c116659961a89b6fbb9a23ed68b11198b792d
Kate Coppess <kcoppess@stanford.edu>
2017-08-08 12:40:22 -0700

    added derivative for circular sequence

--- toy.py
+++ toy.py
@@ -1,4 +1,4 @@
-# last modified: August 7, 2017
+# last modified: August 8, 2017
 # version 2.0
 # toy model to calculate partition function for a given sequence
 # based off of NUPACK pseudo-code (N^4) given in Dirks & Pierce 2003
@@ -87,7 +87,7 @@ def linear_sequence_derivatives(g_base_pair, g_loop, g_stack, N, g): # g : param
     Qb = np.zeros((N,N))
     dQb = np.zeros((N,N)) # stores bound derivatives
     
-    h = 0.000001 # differentiation step size
+    h = 0.00000001 # differentiation step size
 
     # LINEAR SEQUENCE calculation of partition function and its derivative
     for l in range(1,N+1): # iterating over all subsequence lengths
@@ -182,6 +182,91 @@ def circular_sequence_partition(g_base_pair, g_loop, g_stack, N):
                             Q[i,j] += Q[i,d-1]*Qb[d,e]
     return Q[0,N-1]
 
+
+def circular_sequence_derivatives(g_base_pair, g_loop, g_stack, N, g):
+    Q = np.zeros((N,N))
+    for m in range(N):
+        Q[m,m-1] = 1.0
+
+    dQ = np.zeros((N,N))
+
+    Qb = np.zeros((N,N))
+    dQb = np.zeros((N,N))
+
+    h = 0.00000001
+
+    for l in range(1, N+1): #length of subsequence
+        for i in range(0, N-l+1): # start of subsequence
+            j = i + l - 1
+            if j - i > 3 and (i + N) - j > 3 and g_base_pair[i,j]: # checking that base pair can form and bases are at least 4 positions apart on both sides
+                Qb[i,j] = Q_hairpin(g_base_pair[i,j], g_loop)
+                if g == g_base_pair[i,j]:
+                    dQb[i,j] = (Q_hairpin(g_base_pair[i,j] + h, g_loop) - Q_hairpin(g_base_pair[i,j], g_loop))/h
+                elif g == g_loop:
+                    dQb[i,j] = (Q_hairpin(g_base_pair[i,j], g_loop + h) - Q_hairpin(g_base_pair[i,j], g_loop))/h
+            else:  # hairpin not possible
+                Qb[i,j] = 0.0
+            for d in range(i+1,j-4):
+                for e in range(d+4,j):
+                    interior_loop_type = ''
+                    if j - i > 3 and (i + N) - j > 3 and (d + N) - e > 3: #checking that the bases in each base pair are at least 4 positions apart
+                        if g_base_pair[i,j] and g_base_pair[d,e]: # interior loop possible
+                            if i+1 == d and e+1 == j: #stacked
+                                interior_loop_type = 's' #g_interior = g_base_pair[i,j] + g_stack
+                            else: #loop
+                                interior_loop_type = 'l' #g_interior = g_base_pair[i,j] + g_loop
+                            Qb[i,j] += Qb[d,e] * Q_interior(g_base_pair[i,j], g_loop, g_stack, interior_loop_type)
+                            dQ_int = 0.0
+                            if g == g_base_pair[i,j]:
+                                dQ_int = (Q_interior(g_base_pair[i,j] + h, g_loop, g_stack, interior_loop_type) - Q_interior(g_base_pair[i,j], g_loop, g_stack, interior_loop_type))/h
+                            elif g == g_loop and interior_loop_type == 'l':
+                                dQ_int = (Q_interior(g_base_pair[i,j], g_loop + h, g_stack, interior_loop_type) - Q_interior(g_base_pair[i,j], g_loop, g_stack, interior_loop_type))/h
+                            elif g == g_stack and interior_loop_type == 's':
+                                dQ_int = (Q_interior(g_base_pair[i,j], g_loop, g_stack + h, interior_loop_type) - Q_interior(g_base_pair[i,j], g_loop, g_stack, interior_loop_type))/h
+                            dQb[i,j] += dQ_int * Qb[d,e] + Q_interior(g_base_pair[i,j], g_loop, g_stack, interior_loop_type) * dQb[d,e]
+                        else: # interior loop not possible
+                            Qb[i,j] += 0.0
+                    else:
+                        Qb[i,j] += 0.0
+            Q[i,j] = 1.0
+            if i == 0 and j == N-1: # closing chain
+                for d in range(0, N-4):
+                    for e in range(d+4,N):
+                        if d == 0:
+                            Q[i,j] += Qb[0,e]*np.exp(-g_loop/(R*T))
+                            if g == g_loop:
+                                dQ[i,j] += dQb[0,e] * np.exp(-g_loop/(R*T)) - Qb[0,e] * np.exp(-g_loop/(R*T))/(R*T)
+                            else:
+                                dQ[i,j] += dQb[0,e] * np.exp(-g_loop/(R*T))
+                        else:
+                            if e == N-1 and Qb[0,d-1] and Qb[d,N-1]: # to account for stacked pair forming when chain is closed
+                                Q[i,j] += (Q[0,d-1] + Qb[0,d-1]*(np.exp(-(g_stack-g_loop)/(R*T)) - 1))*Qb[d,N-1]*np.exp(-g_loop/(R*T))
+                                if g == g_loop:
+                                    dQ[i,j] += (dQ[0,d-1] + dQb[0,d-1]*(np.exp(-(g_stack-g_loop)/(R*T)) -1) + Qb[0,d-1]*(np.exp(-(g_stack-g_loop)/(R*T))/(R*T)))*Qb[d,N-1]*np.exp(-g_loop/(R*T))
+                                    dQ[i,j] += (Q[0,d-1] + Qb[0,d-1]*(np.exp(-(g_stack-g_loop)/(R*T)) - 1))*(dQb[d,N-1]*np.exp(-g_loop/(R*T)) - Qb[d,N-1]*np.exp(-g_loop/(R*T))/(R*T))
+                                elif g == g_stack:
+                                    dQ[i,j] += (dQ[0,d-1] + dQb[0,d-1]*(np.exp(-(g_stack-g_loop)/(R*T)) -1) + Qb[0,d-1]*(-np.exp(-(g_stack-g_loop)/(R*T))/(R*T)))*Qb[d,N-1]*np.exp(-g_loop/(R*T))
+                                    dQ[i,j] += (Q[0,d-1] + Qb[0,d-1]*(np.exp(-(g_stack-g_loop)/(R*T)) - 1))*dQb[d,N-1]*np.exp(-g_loop/(R*T))
+                                else:
+                                    dQ[i,j] += (dQ[0,d-1] + dQb[0,d-1]*(np.exp(-(g_stack-g_loop)/(R*T)) -1))*Qb[d,N-1]*np.exp(-g_loop/(R*T))
+                                    dQ[i,j] += (Q[0,d-1] + Qb[0,d-1]*(np.exp(-(g_stack-g_loop)/(R*T)) - 1))*dQb[d,N-1]*np.exp(-g_loop/(R*T))
+                            else: # to account for interior loop forming when chain is closed
+                                Q[i,j] += Q[i,d-1]*Qb[d,e]*np.exp(-g_loop/(R*T))
+                                if g == g_loop:
+                                    dQ[i,j] += dQ[i,d-1]*Qb[d,e]*np.exp(-g_loop/(R*T)) + Q[i,d-1]*dQb[d,e]*np.exp(-g_loop/(R*T)) - Q[i,d-1]*Qb[d,e]*np.exp(-g_loop/(R*T))/(R*T)
+                                else:
+                                    dQ[i,j] += dQ[i,d-1]*Qb[d,e]*np.exp(-g_loop/(R*T)) + Q[i,d-1]*dQb[d,e]*np.exp(-g_loop/(R*T))
+            else:
+                for d in range(i,j-3):
+                    for e in range(d+4,j+1):
+                        if d == 0:
+                            Q[i,j] += Qb[d,e]
+                            dQ[i,j] += dQb[d,e]
+                        else:
+                            Q[i,j] += Q[i,d-1]*Qb[d,e]
+                            dQ[i,j] += dQ[i,d-1] * Qb[d,e] + Q[i,d-1] * dQb[d,e]
+    return Q[0,N-1], dQ[0,N-1]
+
 sequence = raw_input('Sequence (if circular, end with \'-\'): ')
 N = len(sequence)
 
@@ -201,9 +286,10 @@ for m in range(N):
     for n in range(N):
         g_base_pair[m,n] = free_energy_pair(sequence[m],sequence[n],5.69,6.0,4.09) # bp1, bp2, g_AU, g_GU, g_GC
 
-
-if is_circular:
-    print circular_sequence_partition(g_base_pair, 1.0, -7.09, N) # g_base_pair, g_loop, g_stack, N
-else:
-    #print linear_sequence_partition(g_base_pair, 1.0, -7.09, N)
-    print linear_sequence_derivatives(g_base_pair, 1.0, -7.09, N, 5.69) # g_base_pair, g_loop, g_stack, N, g
+for dg in [5.69, 6.0, 4.09, 1.0, -7.09]:
+    if is_circular:
+        print str(dg)+' '+str(circular_sequence_derivatives(g_base_pair, 1.0, -7.09, N, dg))
+        #print circular_sequence_partition(g_base_pair, 1.0, -7.09, N) # g_base_pair, g_loop, g_stack, N
+    else:
+        #print linear_sequence_partition(g_base_pair, 1.0, -7.09, N)
+        print str(dg)+' '+str(linear_sequence_derivatives(g_base_pair, 1.0, -7.09, N, dg)) # g_base_pair, g_loop, g_stack, N, g
