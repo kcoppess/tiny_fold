ec48dff0d26b893a0095d14410207d30bb3e0a7a
Kate Coppess <kcoppess@stanford.edu>
2017-08-09 21:07:43 -0700

    w/ learning but model dumb and buggy

--- random_sequence_generator.py
+++ random_sequence_generator.py
@@ -1,12 +1,35 @@
-# generates random RNA sequences
+# modified: August 9, 2017
+# generates random RNA sequences and partition functions
 import random
-
-N = 5
+import partition as z
+import numpy as np
 
 f = open('sequences.txt','w')
 
-for i in range(10):
-    sequence = ''.join(random.choice('AUGC') for _ in range(N))
-    f.write(str(sequence)+'\n')
+training_data = []
+for i in range(100):
+    sequence = ''.join(random.choice('AUGC') for _ in range(random.randint(5,21)))
+    N = len(sequence)
+    datum = []
+    datum.append(sequence)
+    
+    is_circular = False
+    if sequence[-1] == '-':
+        is_circular = True
+        N = N - 1 # ignoring the '-' part of sequence
+        
+    # initializing base-pair free energy matrix
+    # IGNORING STERIC EFFECTS
+    g_base_pair = np.zeros((N,N))                                                    
+    for m in range(N):
+        for n in range(N):
+            g_base_pair[m,n] = z.free_energy_pair(sequence[m],sequence[n],5.69,6.0,4.09) # bp1, bp2, g_AU, g_GU, g_GC
+                                                                             
+    if is_circular:
+        datum.append(z.circular_sequence_partition(g_base_pair, 1.0, -7.09, N)) # g_base_pair, g_loop, g_stack, N
+    else:
+        datum.append(z.linear_sequence_partition(g_base_pair, 1.0, -7.09, N)) # g_base_pair, g_loop, g_stack, N
+    f.write(str(datum)+'\n')
+    training_data.append(datum)
 
 f.close()
--- toy.py
+++ toy.py
@@ -1,20 +1,24 @@
-# last modified: August 8, 2017
-# version 3.0
+# last modified: August 9, 2017
 # toy model to calculate partition function for a given sequence
 # based off of NUPACK pseudo-code (N^4) given in Dirks & Pierce 2003
 # TODO IGNORING MULTILOOPS
 import numpy as np
+import scipy.optimize as so
 import partition as z
+import random_sequence_generator as r
 '''all free energy parameters in kcal/mol'''
 
+'''
+# generating training data
 f = open('sequences.txt','r')
 
-p = open('training_data.txt','w')
+training_data = np.array(f.readline())
 
 sequence = f.readline()[:-1]
 N = len(sequence)
 while sequence:
-    p.write(str(sequence))
+    datum = []
+    datum.append(sequence)
 
     is_circular = False
     if sequence[-1] == '-':
@@ -29,17 +33,82 @@ while sequence:
             g_base_pair[m,n] = z.free_energy_pair(sequence[m],sequence[n],5.69,6.0,4.09) # bp1, bp2, g_AU, g_GU, g_GC
     
     if is_circular:
-        p.write(' '+str(z.circular_sequence_partition(g_base_pair, 1.0, -7.09, N))) # g_base_pair, g_loop, g_stack, N
-        for dg in [5.69, 6.0, 4.09, 1.0, -7.09]:
-            p.write(' '+str(z.circular_sequence_derivatives(g_base_pair, 1.0, -7.09, N, dg)))
+        datum.append(z.circular_sequence_partition(g_base_pair, 1.0, -7.09, N)) # g_base_pair, g_loop, g_stack, N
     else:
-        p.write(' '+str(z.linear_sequence_partition(g_base_pair, 1.0, -7.09, N))) # g_base_pair, g_loop, g_stack, N
-        for dg in [5.69, 6.0, 4.09, 1.0, -7.09]:
-            p.write(' '+str(z.linear_sequence_derivatives(g_base_pair, 1.0, -7.09, N, dg))) # g_base_pair, g_loop, g_stack, N, g
+        datum.append(z.linear_sequence_partition(g_base_pair, 1.0, -7.09, N)) # g_base_pair, g_loop, g_stack, N
     
-    p.write('\n')
+    training_data.append(datum)
     sequence = f.readline()[:-1]
     N = len(sequence)
 
 f.close()
-p.close()
+'''
+
+training_data = r.training_data
+# sum of square residuals
+# want to minimize to get least squares
+# energy_param = [g_AU,g_GU,g_GC,g_loop_g_stack]
+def ssr(energy_param):
+    sr = 0.0
+    for i in range(len(training_data)):
+        sequence = training_data[i][0]
+        N = len(sequence)
+        x = 0.0
+        
+        is_circular = False
+        if sequence[-1] == '-':
+            is_circular = True
+            N = N - 1 # ignoring the '-' part of sequence
+        
+        # initializing base-pair free energy matrix
+        # IGNORING STERIC EFFECTS
+        g_base_pair = np.zeros((N,N))
+        for m in range(N):
+            for n in range(N):
+                g_base_pair[m,n] = z.free_energy_pair(sequence[m],sequence[n], energy_param[0], energy_param[1], energy_param[2]) # bp1, bp2, g_AU, g_GU, g_GC
+        
+        if is_circular:
+            x = z.circular_sequence_partition(g_base_pair, energy_param[3], energy_param[4], N) # g_base_pair, g_loop, g_stack, N
+        else:
+            x = z.linear_sequence_partition(g_base_pair, energy_param[3], energy_param[4], N) # g_base_pair, g_loop, g_stack, N
+        
+        sr += (training_data[i][1] - x)**2
+    return sr
+
+# gradient of ssr wrt energy parameters
+def ssr_prime(energy_param):
+    sr_p = np.zeros(5)
+    for i in range(len(training_data)):
+        sequence = training_data[i][0]
+        N = len(sequence)
+        x = 0.0
+        dx = np.zeros(5)
+        
+        is_circular = False
+        if sequence[-1] == '-':
+            is_circular = True
+            N = N - 1 # ignoring the '-' part of sequence
+        
+        # initializing base-pair free energy matrix
+        # IGNORING STERIC EFFECTS
+        g_base_pair = np.zeros((N,N))
+        for m in range(N):
+            for n in range(N):
+                g_base_pair[m,n] = z.free_energy_pair(sequence[m],sequence[n], energy_param[0], energy_param[1], energy_param[2]) # bp1, bp2, g_AU, g_GU, g_GC
+        
+        if is_circular:
+            x = z.circular_sequence_partition(g_base_pair, energy_param[3], energy_param[4], N) # g_base_pair, g_loop, g_stack, N 
+            for j in range(5):
+                dx[j] = z.circular_sequence_derivatives(g_base_pair, energy_param[3], energy_param[4], N, energy_param[j]) # g_base_pair, g_loop, g_stack, N, dg
+        else:
+            x = z.linear_sequence_partition(g_base_pair, energy_param[3], energy_param[4], N) # g_base_pair, g_loop, g_stack, N
+            for j in range(5):
+                dx[j] = z.linear_sequence_derivatives(g_base_pair, energy_param[3], energy_param[4], N, energy_param[j])
+        sr_p += -2*(training_data[i][1] - x)*dx
+    return sr_p
+
+
+param = np.array([1,2,3,4,5])
+
+#print so.fmin_bfgs(ssr,param,gtol=1e-6)
+print so.fmin_bfgs(ssr,param,fprime=ssr_prime,gtol=1e-6)
