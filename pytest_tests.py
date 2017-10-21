9b9b6139bba8881b07b4fcdd77b5989d7d2cf616
Andy Watkins <andy.watkins2@gmail.com>
2017-10-20 19:05:02 -0700

    Circular unit tests

--- pytest_tests.py
+++ pytest_tests.py
@@ -10,6 +10,12 @@ seqs = [ "AAAAA",
          "ACCCCGU",
          "AACCCCGUU"
 ]
+circs = ["AAAAA-",
+         "AAAAU-",
+         "ACCCUCCC-",
+         "AAACCCUUUCCC-"
+]
+
 tol = 1e-8
 
 def partition_value_test(seq):
@@ -36,6 +42,22 @@ def log_partition_value_test(seq):
     g_base_pair = gm.generator(seq, p.energies[0], p.energies[1], p.energies[2], len(seq))
     return log_z.linear(g_base_pair, p.g_loop, p.energies[3], len(seq))
 
+def circ_partition_value_test(seq):
+    g_base_pair = gm.generator(seq, p.energies[0], p.energies[1], p.energies[2], len(seq)-1)
+    return z.circular(g_base_pair, p.g_loop, p.energies[3], len(seq)-1)
+def circ_partition_deriv_test_AU(seq):
+    g_base_pair = gm.generator(seq, p.energies[0], p.energies[1], p.energies[2], len(seq)-1)
+    return z.circular_derivatives(g_base_pair, p.g_loop, p.energies[3], len(seq)-1, p.energies[0])
+def circ_partition_deriv_test_GU(seq):
+    g_base_pair = gm.generator(seq, p.energies[0], p.energies[1], p.energies[2], len(seq)-1)
+    return z.circular_derivatives(g_base_pair, p.g_loop, p.energies[3], len(seq)-1, p.energies[1])
+def circ_partition_deriv_test_GC(seq):
+    g_base_pair = gm.generator(seq, p.energies[0], p.energies[1], p.energies[2], len(seq)-1)
+    return z.circular_derivatives(g_base_pair, p.g_loop, p.energies[3], len(seq)-1, p.energies[2])
+def circ_partition_deriv_test_stack(seq):
+    g_base_pair = gm.generator(seq, p.energies[0], p.energies[1], p.energies[2], len(seq)-1)
+    return z.circular_derivatives(g_base_pair, p.g_loop, p.energies[3], len(seq)-1, p.energies[3])
+
 def almost_equal(x, y, epsilon ):
       return abs(x - y) < epsilon 
 
@@ -61,3 +83,20 @@ def test_derivs_GC():
 def test_derivs_stack():
     for seq, val in zip(seqs, [0,0,0,-0.003330650339,-0.039330624165]):
         assert(almost_equal(partition_deriv_test_stack(seq), val, tol))
+
+def test_circ_partition():
+    for seq, val in zip(circs, [1, 1, 1.0000023076971, 1.0003791934327]):
+        assert(almost_equal(circ_partition_value_test(seq), val, tol))
+def test_circ_derivs_AU():
+    for seq, val in zip(circs, [0, 0, -3.8949547054E-06, -0.0016844202181]):
+        assert(almost_equal(circ_partition_deriv_test_AU(seq), val, tol))
+def test_circ_derivs_GU():
+    for seq, val in zip(circs, [0,0,0,0]):
+        assert(almost_equal(circ_partition_deriv_test_GU(seq), val, tol))
+def test_circ_derivs_GC():
+    for seq, val in zip(circs, [0,0,0,0]):
+        assert(almost_equal(circ_partition_deriv_test_GC(seq), val, tol))
+def test_circ_derivs_stack():
+    for seq, val in zip(circs, [0,0,0,-0.001044413431]):
+        assert(almost_equal(circ_partition_deriv_test_stack(seq), val, tol))
+
